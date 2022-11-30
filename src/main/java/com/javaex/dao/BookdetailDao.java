29e08c9864b6c567ab4c8696c319f7a25a99b2a4
seoyun <may-primavera@naver.com>
2022-11-30 13:35:42 +0900

    refact: slf4j 제거

--- src/main/java/com/javaex/dao/BookdetailDao.java
+++ src/main/java/com/javaex/dao/BookdetailDao.java
@@ -1,18 +1,15 @@
 package com.javaex.dao;
 
+import com.javaex.vo.BookdetailVo;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import com.javaex.vo.BookdetailVo;
-import org.springframework.transaction.annotation.Transactional;
-
-@Slf4j
 @Repository
 @Transactional
 public class BookdetailDao {
