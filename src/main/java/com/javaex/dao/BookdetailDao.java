4a79fad5c6391e0b1aa0c5b5994b32ba4b4a8b24
seoyun <may-primavera@naver.com>
2022-12-01 15:04:33 +0900

    refact: 책 상세페이지 bookdetail RestAPI로 리팩토링
    - 책 정보
    - 서평 리스트 최신순
    - 서평 리스트 인기순
    - 서평 총 개수

--- src/main/java/com/javaex/dao/BookdetailDao.java
+++ src/main/java/com/javaex/dao/BookdetailDao.java
@@ -1,127 +1,90 @@
 package com.javaex.dao;
 
-import com.javaex.vo.BookdetailVo;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.javaex.dto.bookdetail.BookDetailInfo;
+import com.javaex.dto.bookdetail.BookReviewDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+@Lazy
+@Mapper
 @Repository
-@Transactional
-public class BookdetailDao {
-	
-	@Autowired(required = false)
-	private SqlSession sqlSession;
-	
+public interface BookdetailDao {
 
-	/* 서평 리스트 (최신순) */
-	public List<BookdetailVo> getReviewList(String bookNo) {
+    /* 서평 리스트 (최신순) */
+    List<BookReviewDto> reviewListLatest(String bookNo);
 
-		System.out.println("Dao.getReviewList");
-		List<BookdetailVo> reviewList = sqlSession.selectList("bookdetail.reviewListLatest",bookNo);
-		System.out.println(reviewList);
+    /* 서평 리스트 (인기순) */
+    List<BookReviewDto> reviewListBest(String bookNo);
 
-		return reviewList;
+    /* 해당 책 정보 */
+    BookDetailInfo getbookVo(String bookNo);
 
-	}
-	
-	/* 서평 리스트 (인기순) */
-	public List<BookdetailVo> getReviewBest(String bookNo) {
-		
-		System.out.println("Dao.getReviewBest");
-		List<BookdetailVo> reviewBest = sqlSession.selectList("bookdetail.reviewListBest",bookNo);
-		System.out.println("dao.인기순리스트:"+reviewBest);
-		
-		return reviewBest;
-	}
-	
-	
-	/* 해당 책 정보 */
-	public BookdetailVo getBookVo(String bookNo) {
+    /* 해당 책 총 서평 수 */
+    int bookReviewCount(String bookNo);
 
-		System.out.println("Dao.getBookVo");
-		BookdetailVo bookVo = sqlSession.selectOne("bookdetail.getbookVo",bookNo);
-		System.out.println("dao: "+bookVo);
+    /* 로딩 북마크 체크 */
+    /*public int bookmarkCheck(String bookNo, String userNo) {
 
-		return bookVo;
+        System.out.println("Dao.bookmarkCheck");
 
-	}
-	
-	/* 해당 책 총 서평 수 */ 
-	public int getCount(String bookNo) {
+        Map<String, String> bookmark = new HashMap<String, String>();
+        bookmark.put("userNo", userNo);
+        bookmark.put("bookNo", bookNo);
 
-		System.out.println("Dao.getCount");
-		int getCount = sqlSession.selectOne("bookdetail.bookReviewCount",bookNo);
-		System.out.println(getCount);
+        System.out.println("dao:" + bookmark);
 
-		return getCount;
+        int bookmarkCheck = sqlSession.selectOne("bookdetail.bookmarkCheck", bookmark);
+        System.out.println(bookmarkCheck);
 
-	}
-	
-	/* 로딩 북마크 체크 */
-	public int bookmarkCheck(String bookNo, String userNo) {
-		
-		System.out.println("Dao.bookmarkCheck");
-		
-		Map<String, String> bookmark = new HashMap<String, String>();
-		bookmark.put("userNo", userNo);
-		bookmark.put("bookNo", bookNo);
-		
-		System.out.println("dao:"+bookmark);
-		
-		int bookmarkCheck = sqlSession.selectOne("bookdetail.bookmarkCheck",bookmark);
-		System.out.println(bookmarkCheck);
-		
-		return bookmarkCheck;
-	}
-	
-	/* 북마크 추가*/ 
-	public int bookmarkInsert(String userNo, String bookNo) {
-		
-		System.out.println("Dao.bookmarkInsert");
-		
-		Map<String, String> markInsert = new HashMap<String, String>();
-		markInsert.put("userNo", userNo);
-		markInsert.put("bookNo", bookNo);
-		
-		int addCount = sqlSession.insert("bookdetail.bookmarkInsert",markInsert);
-		System.out.println("addCount:"+addCount);
-		
-		return addCount;
-	}
-	
-	
-	/* 북마크 제거 */
-	public int bookmarkDelete(String userNo, String bookNo) {
-		
-		System.out.println("Dao.bookmarkDelete");
-		
-		Map<String, String> markDelete = new HashMap<String, String>();
-		markDelete.put("userNo", userNo);
-		markDelete.put("bookNo", bookNo);
-		
-		int deleteCount = sqlSession.delete("bookdetail.bookmarkDelete",markDelete);
-		System.out.println("deleteCount:"+deleteCount);
+        return bookmarkCheck;
+    }
 
-		
-		return deleteCount;
-		
-	}
-	
-	
-	/* 서평 삭제 */
-	public int reviewDelete(int reviewNo) {
-		System.out.println("Dao.reviewDelete");
-		
-		int deleteResult = sqlSession.delete("bookdetail.reviewDelete",reviewNo);		
-		System.out.println("책상세 서평 삭제 결과:"+deleteResult);
-		
-		return deleteResult;
-	}
-	
+    *//* 북마크 추가*//*
+    public int bookmarkInsert(String userNo, String bookNo) {
+
+        System.out.println("Dao.bookmarkInsert");
+
+        Map<String, String> markInsert = new HashMap<String, String>();
+        markInsert.put("userNo", userNo);
+        markInsert.put("bookNo", bookNo);
+
+        int addCount = sqlSession.insert("bookdetail.bookmarkInsert", markInsert);
+        System.out.println("addCount:" + addCount);
+
+        return addCount;
+    }
+
+
+    *//* 북마크 제거 *//*
+    public int bookmarkDelete(String userNo, String bookNo) {
+
+        System.out.println("Dao.bookmarkDelete");
+
+        Map<String, String> markDelete = new HashMap<String, String>();
+        markDelete.put("userNo", userNo);
+        markDelete.put("bookNo", bookNo);
+
+        int deleteCount = sqlSession.delete("bookdetail.bookmarkDelete", markDelete);
+        System.out.println("deleteCount:" + deleteCount);
+
+
+        return deleteCount;
+
+    }
+
+
+    *//* 서평 삭제 *//*
+    public int reviewDelete(int reviewNo) {
+        System.out.println("Dao.reviewDelete");
+
+        int deleteResult = sqlSession.delete("bookdetail.reviewDelete", reviewNo);
+        System.out.println("책상세 서평 삭제 결과:" + deleteResult);
+
+        return deleteResult;
+    }*/
+
 
 }
