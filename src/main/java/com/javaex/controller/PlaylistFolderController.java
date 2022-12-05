f59328089084be96ff14e8a04f7dfc9ca367bfaa
seoyun <may-primavera@naver.com>
2022-12-06 01:19:03 +0900

    refact: playlistFolder Mapping + repository mapper 리팩토링

--- src/main/java/com/javaex/controller/PlaylistFolderController.java
+++ src/main/java/com/javaex/controller/PlaylistFolderController.java
@@ -1,142 +1,109 @@
 package com.javaex.controller;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import com.javaex.dto.playlistFolder.PlaylistFolderModalResponse;
+import com.javaex.dto.playlistFolder.PlaylistFolderResponse;
+import com.javaex.dto.playlistFolder.PlaylistFolderSearchDto;
+import com.javaex.dto.playlistFolder.PlaylistLikeDto;
 import com.javaex.service.PlaylistFolderService;
 import com.javaex.vo.PlaylistFolderVo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/playlist")
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/boggle/playlist")
 public class PlaylistFolderController {
-	
-	@Autowired
-	private PlaylistFolderService playlistfolderService;
-	
-	/* 플레이리스트 폴더 클릭 -> 해당 플레이리스트 서평 리스트+페이징  */
-	@RequestMapping("/folder")
-	public String playlistFolder(@RequestParam("playlistNo") int playlistNo,
-								 @RequestParam("userNo") int userNo,
-								 @RequestParam(value = "crtPage", required = false, defaultValue = "1") int crtPage,
-								 Model model) {
-		
-		System.out.println("Controller.playlistFolder+paging");
-		System.out.println(playlistNo);
-		System.out.println("요청 페이지:"+crtPage);
+    private final PlaylistFolderService playlistfolderService;
 
-		//플레이리스트 번호 받기
-		//해당 폴더 서평리스트 가져오기(5개)
-		Map<String, Object> foldermainMap = playlistfolderService.playlistReviewList(playlistNo,userNo,crtPage);
-		System.out.println("controller: "+foldermainMap);
-		
-		//데이터 보내기
-		model.addAttribute("foldermainMap",foldermainMap);
-		
-		return "taste/click-playlist";
-		
-	}
-	
-	/* 플리 모달 페이징+리스트 */
-	@ResponseBody
-	@RequestMapping("/modalListPage")
-	public Map<String, Object> modalListPage(@RequestParam(value = "crtPage", required = false, defaultValue = "1") int crtPage) {
-		
-		System.out.println("Controller.modalListPage");
-		System.out.println(crtPage);
-		
-		//해당페이지의 글 리스트 5개
-		Map<String, Object> playlistMap = playlistfolderService.madalListPage(crtPage);
-		System.out.println(playlistMap);
+    /* 플레이리스트 폴더 클릭 -> 해당 플레이리스트 서평 리스트+페이징  */
+    @GetMapping("/{playlist-no}/folder")
+    public ResponseEntity<PlaylistFolderResponse> playlistFolder(@PathVariable("playlist-no") Long playlistNo,
+                                                                 @RequestParam(value = "crtPage", required = false, defaultValue = "1") int crtPage) {
 
-		return playlistMap;
-	}
-	
-	
-	/* 플리 모달 검색창 */
-	@ResponseBody
-	@RequestMapping("/reviewSearch")
-	public List<PlaylistFolderVo> reviewSearch(@RequestParam(value="SearchTxt") String searchTxt ) {
-		
-		System.out.println("Controller.reviewSearch");
-		System.out.println(searchTxt);
-		
-		List<PlaylistFolderVo> searchResult = playlistfolderService.getSearchResult(searchTxt);
-		System.out.println("컨트롤러:"+searchResult);
-		
-		return searchResult;
-	}
-	
-	/* 플리 모달 추가 선택 등록 */
-	@RequestMapping("/addReviews")
-	public @ResponseBody int addReviews(@ModelAttribute PlaylistFolderVo playlistFolderVo, 
-										   @RequestParam("userNo") int userNo,
-										   @RequestParam (value="reviewChkBoxArr[]") List<Integer> reviewChkBoxArr) {
-											
-		System.out.println("Controller.배열");
-		System.out.println(playlistFolderVo);
-		System.out.println(reviewChkBoxArr);
+        log.info("{} + paging : {}", playlistNo, crtPage);
 
-		int addResult = playlistfolderService.reviewsInsert(playlistFolderVo, reviewChkBoxArr);
-		
-		return addResult;
-	
-	}
-	
-	/* 서평 삭제 */
-	@ResponseBody
-	@RequestMapping("/reviewDelete")
-	public int reviewRemove(@RequestParam("reviewNo") int reviewNo) {
-		
-		System.out.println("Controller.reviewDelete");
-		System.out.println(reviewNo);
-		
-		int deleteResult = playlistfolderService.reviewDelete(reviewNo);
-		
-		return deleteResult;
-		
-	}
-	
-	/* 로딩시 플리 좋아요 체크 */
-	@ResponseBody
-	@RequestMapping("/checkLike")
-	public int checkLike(@ModelAttribute PlaylistFolderVo playlistFolderVo) {
-		
-		System.out.println("Controller.checkLike");
-		System.out.println(playlistFolderVo);
-		
-		int checkLike = playlistfolderService.checkLike(playlistFolderVo);
-		
-		return checkLike;
-	}
-	
-	/* 해당 플리 좋아요 취소 */
-	@ResponseBody
-	@RequestMapping("/playlistUnlike")
-	public int playlistUnlike(@ModelAttribute PlaylistFolderVo playlistFolderVo) {
-		
-		System.out.println("Controller.playlistUnlike");
-		int unlikeResult = playlistfolderService.playlistUnlike(playlistFolderVo);
-		
-		return unlikeResult;
-	}
-	
-	/* 해당 플리 좋아요 */
-	@ResponseBody
-	@RequestMapping("/addplaylistLike")
-	public int addplaylistLike(@ModelAttribute PlaylistFolderVo playlistFolderVo) {
-		
-		System.out.println("Controller.addplaylistLike");
-		int likeResult = playlistfolderService.playlistlike(playlistFolderVo);
-		
-		return likeResult;
-	}
+        //플레이리스트 번호 받기
+        //해당 폴더 서평리스트 가져오기(5개)
+        PlaylistFolderResponse playlistFolderResponse = playlistfolderService.playlistReviewList(playlistNo, crtPage);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(playlistFolderResponse);
+    }
+
+    /* 플리 모달 페이징+리스트 */
+    @GetMapping("/modal")
+    public ResponseEntity<PlaylistFolderModalResponse> modalListPage(@RequestParam(value = "crtPage", required = false, defaultValue = "1") int crtPage) {
+        log.info("modalListPage 요청 페이지: {}", crtPage);
+        //해당페이지의 글 리스트 5개
+        PlaylistFolderModalResponse playlistFolderModalResponse = playlistfolderService.madalListPage(crtPage);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(playlistFolderModalResponse);
+    }
+
+    /* 플리 모달 검색창 */
+    @GetMapping("/review-search")
+    public ResponseEntity<List<PlaylistFolderSearchDto>> reviewSearch(@RequestParam(value = "search") String searchTxt) {
+        log.info("reviewSearch: {}", searchTxt);
+        List<PlaylistFolderSearchDto> searchResult = playlistfolderService.getSearchResult(searchTxt);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(searchResult);
+    }
+
+    /* 플리 모달 추가 선택 등록 *///TODO
+    @PutMapping("/{playlist-no}/reviews")
+    public ResponseEntity addReviews(@PathVariable("playlist-no") Long playlistNo,
+                                     @RequestBody List<Integer> reviewChkArr) {// reviewNo list
+        log.info("선택한 서평:{}", reviewChkArr);
+        int addResult = playlistfolderService.reviewsInsert(playlistNo, reviewChkArr);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("등록 성공");
+    }
+
+    /* 서평 삭제 */
+    @DeleteMapping("/review/{review-no}")
+    public int reviewRemove(@PathVariable("review-no") Long reviewNo) {
+        log.info("{}: reviewDelete", reviewNo);
+        int deleteResult = playlistfolderService.reviewDelete(reviewNo);
+        return deleteResult;
+    }
+
+    /* 로딩시 플리 좋아요 체크 *///TODO: enum type 변경
+    @GetMapping("{playlist-no}/like")
+    public ResponseEntity checkLike(@PathVariable("playlist-no") Long playlistNo,
+                         @RequestParam("user-no") Long userNo) {
+        log.info("{}플리 checkLike, user: {}", playlistNo, userNo);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(playlistfolderService.checkLike(playlistNo, userNo));
+    }
+
+    /* 해당 플리 좋아요 취소 *///TODO: enum
+    @DeleteMapping("/like")
+    public ResponseEntity playlistUnlike(@RequestBody PlaylistLikeDto playlistLikeDto) {
+        log.info("playlistUnlike {}", playlistLikeDto);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(playlistfolderService.playlistUnlike(playlistLikeDto));
+    }
+
+    /* 해당 플리 좋아요 */
+    @PutMapping("/like")
+    public ResponseEntity playlistLike(@RequestBody PlaylistLikeDto playlistLikeDto) {
+        log.info("playlistLike {}", playlistLikeDto);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(playlistfolderService.playlistLike(playlistLikeDto));
+    }
 
 }
--- src/main/java/com/javaex/dao/PlaylistFolderDao.java
+++ src/main/java/com/javaex/dao/PlaylistFolderDao.java
@@ -1,153 +1,53 @@
 package com.javaex.dao;
 
-import java.util.HashMap;
+import com.javaex.dto.playlistFolder.PlaylistCoverDto;
+import com.javaex.dto.playlistFolder.PlaylistFolderSearchDto;
+import com.javaex.dto.playlistFolder.PlaylistLikeDto;
+import com.javaex.vo.PlaylistFolderVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import com.javaex.vo.PlaylistFolderVo;
-
+@Lazy
+@Mapper
 @Repository
-public class PlaylistFolderDao {
+public interface PlaylistFolderDao {
 
-	@Autowired(required = false)
-	private SqlSession sqlSession;
-	
-	/* 플레이리스트 폴더 클릭 -> 해당 플레이리스트 서평 리스트  */
-	public List<PlaylistFolderVo> playlistReviewList(int playlistNo,int startRnum, int endRnum) {
-		
-		System.out.println("Dao.playlistReviewList");
-		
-		//map 
-		Map<String, Object> playlistData = new HashMap<String, Object>();
-		playlistData.put("playlistNo", playlistNo);
-		playlistData.put("startRnum", startRnum);
-		playlistData.put("endRnum", endRnum);
-		
-		//서평리스트
-		List<PlaylistFolderVo> playList = sqlSession.selectList("playlistFolder.reviewList",playlistData);
-		System.out.println("dao:"+playList);
+    /* 플레이리스트 폴더 클릭 -> 해당 플레이리스트 서평 리스트  */
+    List<PlaylistFolderVo> reviewList(Map<String, Object> ReviewPageCnt);
 
-		return playList;
-		
-	}
-	
-	/* 해당 플리(folder) 총 서평 수  */
-	public int folderReviewTotal(int playlistNo) {
-		System.out.println("Dao.folderReviewTotal");
-		int folderReviewCnt = sqlSession.selectOne("playlistFolder.folderReviewCnt",playlistNo);
-		System.out.println(folderReviewCnt);
-		return folderReviewCnt;
-	}
+    /* 플레이리스트 커버 */
+    PlaylistCoverDto playlistCover(Long playlistNo);
 
-	/* 플레이리스트 커버 */
-	
-	  public PlaylistFolderVo playlistCover(int playlistNo, int userNo) {
-	  
-	  System.out.println("Dao.playlistCover");
-	  
-	  Map<String, Integer> playlist = new HashMap<String, Integer>();
-	  playlist.put("playlistNo", playlistNo); 
-	  playlist.put("userNo", userNo);
-	  
-	  //playlistCover 
-	  PlaylistFolderVo playlistCover = sqlSession.selectOne("playlistFolder.playlistCover",playlist);
-	  System.out.println(playlistCover);
+    /* 해당 플리(folder) 총 서평 수  */
+    Long folderReviewCnt(Long playlistNo);
 
-	  return playlistCover;
-	  
-	  }
+    /* 전체 글갯수 가져오기 */
+    Long totalCnt();
 
-	/* 플리 모달 페이징 */
-	public List<PlaylistFolderVo> madalListPage(int startRnum, int endRnum) {
-		
-		System.out.println("Dao.modalPage");
-		System.out.println(startRnum+","+endRnum);
-		
-		Map<String,Integer> map = new HashMap<String,Integer>();
-		map.put("startRnum", startRnum);
-		map.put("endRnum", endRnum);
-		
-		List<PlaylistFolderVo> playlistPage = sqlSession.selectList("playlistFolder.modalPage",map);
-		System.out.println(playlistPage);
-		
-		return playlistPage;
-	}
-	
-	/* 전체 글갯수 가져오기 */
-	public int selectTotal() {
-		System.out.println("Dao.selectTotal");
-		int totalCnt = sqlSession.selectOne("playlistFolder.totalCnt");
-		return totalCnt;
-		
-	}
-	
-	/* 플리 검색 결과 가져오기 */
-	public List<PlaylistFolderVo> getSearchResult(String search) {
-		
-		System.out.println("Dao.getSearchResult");
-		List<PlaylistFolderVo> searchResult = sqlSession.selectList("playlistFolder.playlistSearch",search);
-		
-		System.out.println("검색 결과 : "+searchResult);
-		
-		return searchResult;
-	}
-	
-	/* 선택 서평 등록하기 */
-	public int reviewsInsert(PlaylistFolderVo playlistFolderVo) {
-		
-		System.out.println("Dao.reviewsInsert");
-		int addVo = sqlSession.insert("playlistFolder.reviewsInsert",playlistFolderVo);
-		System.out.println("등록 성공:"+addVo);
-		
-		return addVo;
-	}
-	
-	/* 서평 삭제 */
-	public int reviewDelete(int reviewNo) {
-		
-		System.out.println("Dao.reviewDelete");
-		
-		int deleteResult = sqlSession.delete("playlistFolder.reviewDelete",reviewNo);
-		System.out.println("삭제 성공 여부:"+deleteResult);
-		return deleteResult;
-		
-	}
-	
-	/* 로딩시 좋아요 체크 */
-	public int checkLike(PlaylistFolderVo playlistFolderVo) {	
-		
-		System.out.println("Dao.checkLike");
-		int checkLike = sqlSession.selectOne("playlistFolder.checkLike",playlistFolderVo);
-		System.out.println("좋아요 체크:"+checkLike);
-		
-		return checkLike;
-		
-	}
-	
-	/* 해당 플리 좋아요 취소 */
-	public int playlistUnlike(PlaylistFolderVo playlistFolderVo) {
-		
-		System.out.println("Dao.playlistUnlike");
-		int unlikeResult = sqlSession.delete("playlistFolder.playlistUnlike",playlistFolderVo);
-		System.out.println("좋아요 취소:"+unlikeResult);
-		
-		return unlikeResult;
-		
-	}
-	
-	/* 해당 플리 좋아요 */
-	public int playlistlike(PlaylistFolderVo playlistFolderVo) {
-		System.out.println("Dao.playlistlike");
-		int likeResult = sqlSession.insert("playlistFolder.playlistlike",playlistFolderVo);
-		System.out.println("플리 좋아요:"+likeResult);
-		
-		return likeResult;
-	}
-	
-	
-	
+    /* 플리 모달 페이징 */
+    List<PlaylistFolderVo> madalListPage(Map<String, Integer> pageStartEndMa);
+
+    /* 플리 검색 결과 가져오기 */
+    List<PlaylistFolderSearchDto> playlistSearch(String search);
+
+    /* 선택 서평 등록하기 */
+    int reviewsInsert(Map<String, Object> reviewInsertMap);
+
+    /* 서평 삭제 */
+    int reviewDelete(Long reviewNo);
+
+    /* 로딩시 좋아요 체크 */
+    int checkLike(Map<String, Long> likeMap);
+
+    /* 해당 플리 좋아요 취소 */
+    int playlistUnlike(PlaylistLikeDto playlistLikeDto);
+
+    /* 해당 플리 좋아요 */
+    int playlistLike(PlaylistLikeDto playlistLikeDto);
+
+
 }
